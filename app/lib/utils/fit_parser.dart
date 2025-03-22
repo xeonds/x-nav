c2d30367085ddca8c0bf8d0c507dacdbf1050c83
xeonds <xeonds@stu.xidian.edu.cn>
2025-03-23 02:50:48 +0800

    feat: 更新 FitParser，替换 Google Maps 依赖为 latlong2

--- app/lib/utils/fit_parser.dart
+++ app/lib/utils/fit_parser.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 import 'package:fit_parser/fit_parser.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:latlong2/latlong.dart';
 
 class FitParser {
   static List<Map<String, dynamic>> parseFitFiles(String directoryPath) {
